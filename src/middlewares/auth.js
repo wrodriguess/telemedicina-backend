1c82c5434c379dff5555c807147a0bdfea368447
William <willrodriguess77@gmail.com>
2022-04-05 17:19:21 -0300

    teste

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -6,7 +6,7 @@ module.exports = (req, res, next) => {
     const authHeader = req.headers.authorization
 
     if (!authHeader) {
-      return res.status(401).json({ error: 'Token não informado'})
+      return res.status(401).json({ error: ':D Token não informado'})
     }
 
     const parts = authHeader.split(' ')
